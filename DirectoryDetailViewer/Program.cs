f75f88b6667b0744cc6d57db2380a9d34ec9c692
mik-claire <mekakushi.mkz@gmail.com>
2016-02-18 01:12:02 +0900

    First commit.

---
+++ DirectoryDetailViewer/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirectoryDetailViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter full directory path.");
+            string directoryPath = Console.ReadLine();
+
+            while (directoryPath != "exit")
+            {
+                if (!existsDirectory(directoryPath))
+                {
+                    Console.WriteLine("The requested directory was NOT found.");
+                    Console.WriteLine();
+                    Console.WriteLine("Please enter full directory path.");
+                    directoryPath = Console.ReadLine();
+                    continue;
+                }
+
+                size = 0;
+                count = 0;
+                addByteSize(directoryPath);
+
+                Console.WriteLine("Total File Size : {0} byte", size.ToString("#,0"));
+                Console.WriteLine("Total File Count: {0}", count.ToString("#,0"));
+
+                Console.WriteLine();
+                Console.WriteLine("Please enter full directory path.");
+                directoryPath = Console.ReadLine();
+            }
+
+            Console.WriteLine("Please enter any key to exit...");
+            Console.ReadLine();
+        }
+
+        private static bool existsDirectory(string directoryPath)
+        {
+            string path = directoryPath.Trim();
+            if (path == string.Empty)
+            {
+                return false;
+            }
+
+            if (!Directory.Exists(path))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool existsFile(string filePath)
+        {
+            string path = filePath.Trim();
+            if (path == string.Empty)
+            {
+                return false;
+            }
+
+            if (!File.Exists(path))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static long size = 0;
+        private static int count = 0;
+        private static void addByteSize(string directoryPath)
+        {
+            if (!existsDirectory(directoryPath))
+            {
+                return;
+            }
+
+            DirectoryInfo root = new DirectoryInfo(directoryPath);
+
+            var dis = root.GetDirectories();
+            if (0 < dis.Length)
+            {
+                foreach(var di in dis)
+                {
+                    if (!existsDirectory(di.FullName))
+                    {
+                        continue;
+                    }
+
+                    addByteSize(di.FullName);
+                }
+            }
+
+            var fis = root.GetFiles();
+            foreach(var fi in fis)
+            {
+                if (!existsFile(fi.FullName))
+                {
+                    continue;
+                }
+
+                size += fi.Length;
+                count++;
+            }
+        }
+    }
+}
